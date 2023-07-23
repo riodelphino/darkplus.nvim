580296869b66639bbbb2fbb49ede2fbd1ec8a0a5
riodelphino <ryo.heavenlysky@gmail.com>
2023-07-23 23:43:16 +0900

    headings

--- lua/darkplus/theme.lua
+++ lua/darkplus/theme.lua
@@ -219,9 +219,9 @@ theme.set_highlights = function()
    -- "#6a9955"
    hl(0, "@text.title", { link = "" })                   -- title text after #. This should be unset the link, so the below codes work.
    hl(0, "@punctuation.special", { fg = c.ui_orange })   -- # ## ### #### ...
-   hl(0, "@text.title.1", { fg = c.orange, underline = true, bold = true })
-   hl(0, "@text.title.2", { fg = c.orange, bold = true })
-   hl(0, "@text.title.3", { fg = c.orange })
+   hl(0, "@text.title.1", { fg = c.ui_orange, underline = true, bold = true })
+   hl(0, "@text.title.2", { fg = c.ui_orange, bold = true })
+   hl(0, "@text.title.3", { fg = c.ui_orange })
    hl(0, "@text.title.4", { fg = c.ui_orange })
    hl(0, "@text.title.5", { fg = c.ui_orange })
    hl(0, "@text.title.6", { fg = c.ui_orange })
