dbb8e39d0fed3568d3878a7de4daf527dad25ae7
riodelphino <ryo.heavenlysky@gmail.com>
2023-07-23 19:22:00 +0900

    table title

--- after/queries/markdown/highlights.scm
+++ after/queries/markdown/highlights.scm
@@ -1,2 +1,37 @@
 ;; extends
 ((atx_heading) (inline) @text.title)
+
+; Headings
+(atx_heading
+  (atx_h1_marker)
+  (inline) @test @text.underline
+)
+
+; Code block
+(fenced_code_block
+  (fenced_code_block_delimiter) @iio @conceal ; Not works.
+)
+(fenced_code_block_delimiter) @iio @conceal ; Not works.
+[
+  (fenced_code_block_delimiter)
+] @conceal ; Works !
+
+(fenced_code_block
+  (info_string) @pio @conceal
+)
+
+
+; Block quote
+(block_quote) @comment
+
+; Table
+(pipe_table
+  (pipe_table_header) @table.header
+)
+(pipe_table
+  (pipe_table_header
+    (pipe_table_cell) @table.header.cell
+  )
+)
+
+
