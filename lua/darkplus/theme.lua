e167c9035a3ebf1ccc52ca5f9d58d80bf17954ac
riodelphino <ryo.heavenlysky@gmail.com>
2024-12-19 00:54:13 +0900

    Uncomment @markup.list.*

--- lua/darkplus/theme.lua
+++ lua/darkplus/theme.lua
@@ -284,7 +284,7 @@ theme.set_highlights = function()
    hl(0, "@markup.list.checked.text", { fg = c.gray })
    hl(0, "@markup.list.unchecked", { fg = c.magenta, bold = true })      -- [ ] task list unchecked
    hl(0, "@markup.list.unchecked.text", { fg = c.fg })
-   hl(0, "@markup.list.pending", { fg = c.magenta, bold = true })           -- [-] task list pending (TS doesn't parse this, so cannot markup. NEED custom parser.)
+   hl(0, "@markup.list.pending", { fg = c.magenta, bold = true })        -- [-] task list pending (TS doesn't parse this, so cannot markup. NEED custom parser.)
    hl(0, "@markup.list.pending.text", { fg = c.fg })
    hl(0, "@markup.raw", { fg = c.orange }) -- `the raw code`
 
