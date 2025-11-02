ed6f05c6c6313a75186d694d1ae012c674326ee8
riodelphino <ryo.heavenlysky@gmail.com>
2025-11-03 00:44:31 +0900

    fix: Concat generals into generals.lua

--- lua/darkplus/highlights/builtins/mode.lua
+++
@@ -1,6 +0,0 @@
-local c = require('darkplus.palette')
-
-return {
-   Visual = { fg = nil, bg = c.ui_blue },
-   VisualNOS = { fg = nil, bg = c.alt_bg },
-}
--- lua/darkplus/highlights/builtins/symboloutline.lua
+++
@@ -1,7 +0,0 @@
-local c = require('darkplus.palette')
-
--- SymbolOutline
-return {
-   SymbolsOutlineConnector = { fg = c.gray, bg = nil },
-   FocusedSymbol = { fg = nil, bg = '#36383F' },
-}
