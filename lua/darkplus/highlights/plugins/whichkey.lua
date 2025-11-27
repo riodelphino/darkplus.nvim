42e18841eb0c79b3388d2c4dcc0805fb5f750a1a
riodelphino <ryo.heavenlysky@gmail.com>
2025-11-28 02:24:51 +0900

    fix: whichkey bg brighter

--- lua/darkplus/highlights/plugins/whichkey.lua
+++ lua/darkplus/highlights/plugins/whichkey.lua
@@ -6,5 +6,7 @@ return {
    ['WhichKeySeperator'] = { fg = c.green, bg = nil },
    ['WhichKeyGroup'] = { fg = c.blue, bg = nil },
    ['WhichKeyDesc'] = { fg = c.fg, bg = nil },
-   ['WhichKeyFloat'] = { fg = nil, bg = c.alt_bg },
+   ['WhichKeyNormal'] = { fg = c.fg, bg = c.alt_bg2 },
+   ['WhichKeyFloat'] = { fg = c.fg, bg = c.alt_bg2 },
+   ['WhichKeyBorder'] = { fg = c.fg, bg = c.alt_bg2 },
 }
