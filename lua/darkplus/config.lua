1582341606528723690fbbfb89b7fb96799374ff
riodelphino <ryo.heavenlysky@gmail.com>
2026-07-25 15:03:15 +0900

    chore: Split xml

--- lua/darkplus/config.lua
+++ lua/darkplus/config.lua
@@ -43,6 +43,7 @@ local defaults = {
          'sass',
          'json',
          'toml',
+         'xml',
       },
    },
 }
---
+++ lua/darkplus/modules/filetypes/xml.lua
@@ -0,0 +1,8 @@
+local c = require('darkplus.palette').colors
+
+-- xml
+return {
+   ['xmlTag'] = { fg = c.cyan, bg = nil },
+   ['xmlTagName'] = { fg = c.cyan, bg = nil },
+   ['xmlEndTag'] = { fg = c.cyan, bg = nil },
+}
