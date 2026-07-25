a5743e6d9a98bfa69479c6de452c3043f37ece47
riodelphino <ryo.heavenlysky@gmail.com>
2026-07-25 14:56:17 +0900

    feat: Add sass highlights / Split css highlights

---
+++ lua/darkplus/modules/filetypes/css.lua
@@ -0,0 +1,26 @@
+local c = require('darkplus.palette').colors
+
+-- css
+return {
+   ['cssBraces'] = { fg = c.fg, bg = nil },
+   ['cssInclude'] = { fg = c.purple, bg = nil },
+   ['cssTagName'] = { fg = c.light_orange, bg = nil },
+   ['cssClassName'] = { fg = c.yellow, bg = nil },
+   ['cssPseudoClass'] = { fg = c.yellow, bg = nil },
+   ['cssPseudoClassId'] = { fg = c.yellow, bg = nil },
+   ['cssPseudoClassLang'] = { fg = c.yellow, bg = nil },
+   ['cssIdentifier'] = { fg = c.yellow, bg = nil },
+   ['cssProp'] = { fg = c.light_blue, bg = nil },
+   ['cssDefinition'] = { fg = c.fg, bg = nil },
+   ['cssAttr'] = { fg = c.orange, bg = nil },
+   ['cssAttrRegion'] = { fg = c.orange, bg = nil },
+   ['cssColor'] = { fg = c.orange, bg = nil },
+   ['cssFunction'] = { fg = c.purple, bg = nil },
+   ['cssFunctionName'] = { fg = c.yellow, bg = nil },
+   ['cssVendor'] = { fg = c.orange, bg = nil },
+   ['cssValueNumber'] = { fg = c.orange, bg = nil },
+   ['cssValueLength'] = { fg = c.orange, bg = nil },
+   ['cssUnitDecorators'] = { fg = c.orange, bg = nil },
+   ['cssStyle'] = { fg = c.fg, bg = nil },
+   ['cssImportant'] = { fg = c.blue, bg = nil },
+}
