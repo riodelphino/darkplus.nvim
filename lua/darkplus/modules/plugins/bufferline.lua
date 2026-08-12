27b6401e34951389667858ed7138129543045d04
riodelphino <ryo.heavenlysky@gmail.com>
2026-08-12 19:22:14 +0900

    fix: Enable bufferline colors

--- lua/darkplus/modules/plugins/bufferline.lua
+++ lua/darkplus/modules/plugins/bufferline.lua
@@ -1,72 +1,33 @@
 local c = require('darkplus.palette').colors
 
 -- BufferLine
--- return {
---    -- !!! Caution !!!
---    -- BufferLine has original "configs.highlights" options which has to set colors. Not here.
---    -- Otherwise web-dev-icon colors will be lost & the background turns to black.
---
---    ['BufferCurrent'] = { fg = c.fg, bg = c.bg },
---    ['BufferLineFill'] = { fg = c.fg, bg = c.alt_bg },
---    ['BufferLineBackground'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineBufferVisible'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineBufferSelected'] = { fg = c.fg, bg = c.ui_blue, italic = false },
---    ['BufferLineBuffer'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineCloseButton'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineCloseButtonSelected'] = { fg = c.fg, bg = c.ui_blue },
---    ['BufferLineCloseButtonVislble'] = { fg = c.fg, bg = c.tab },
---    -- "BufferLineTab"
---    -- "BufferLineTabSelected"
---    -- "BufferLineTabClose"
---    -- "BufferLineNumbers"
---    -- "BufferLineNumbersVisible"
---    -- "BufferLineNumbersSelected"
---    -- "BufferLineDiagnostic"
---    -- "BufferLineDiagnosticVisible"
---    -- "BufferLineDiagnosticSelected"
---    -- "BufferLineHint"
---    -- "BufferLineHintVisible"
---    -- "BufferLineHintSelected"
---    -- "BufferLineHintDiagnostic"
---    -- "BufferLineHintDiagnosticVisible"
---    -- "BufferLineHintDiagnosticSelected"
---    -- "BufferLineInfo"
---    -- "BufferLineInfoVisible"
---    -- "BufferLineInfoSelected"
---    -- "BufferLineInfoDiagnostic"
---    -- "BufferLineInfoDiagnostic_visible"
---    -- "BufferLineInfoDiagnostic_selected"
---    -- "BufferLineWarning"
---    -- "BufferLineWarningVisible"
---    -- "BufferLineWarningSelected"
---    -- "BufferLineWarningDiagnostic"
---    -- "BufferLineWarningDiagnostic_visible"
---    -- "BufferLineWarningDiagnostic_selected"
---    -- "BufferLineError"
---    -- "BufferLineErrorVisible"
---    -- "BufferLineErrorSelected"
---    -- "BufferLineErrorDiagnostic"
---    -- "BufferLineErrorDiagnostic_visible"
---    -- "BufferLineErrorDiagnostic_selected"
---    ['BufferLineModified'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineModifiedVisible'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineModifiedSelected'] = { fg = c.fg, bg = c.ui_blue },
---    ['BufferLineDuplicateSelected'] = { fg = c.green, bg = c.ui_blue },
---    ['BufferLineDuplicateVisible'] = { fg = c.gray, bg = c.tab },
---    ['BufferLineDuplicate'] = { fg = c.green, bg = c.tab },
---    ['BufferLineSeparator'] = { fg = c.alt_bg, bg = c.alt_bg },
---    ['BufferLineSeparatorSelected'] = { fg = c.alt_bg, bg = c.alt_bg },
---    ['BufferLineSeparatorVisible'] = { fg = c.alt_bg, bg = c.tab },
---    ['BufferLineIndicatorSelected'] = { fg = c.fg, bg = c.ui_blue },
---    ['BufferLineIndicatorVisible'] = { fg = c.fg, bg = c.tab },
---    -- "BufferLinePickSelected"
---    -- "BufferLinePickVisible"
---    -- "BufferLinePick"
---    -- "BufferLineOffsetSeparator"
---    ['BufferLineDevIcon'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineDevIconDefault'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineDevIconVisible'] = { fg = c.fg, bg = c.tab },
---    ['BufferLineDevIconSelected'] = { fg = c.fg, bg = c.ui_blue },
--- }
-
-return {}
+-- NOTE: devicon (filetype icon) foreground colors are generated automatically by
+-- bufferline.nvim itself from nvim-web-devicons, per language, at runtime
+-- (see bufferline/highlights.lua `set_icon_highlight`). We must NOT set
+-- 'BufferLineDevIcon*' groups here -- they aren't the groups actually applied to
+-- icons, and defining them would do nothing useful.
+-- The icon's *background*, however, is inherited from the buffer-state groups
+-- below (BufferLineBuffer/BufferLineBufferVisible/BufferLineBufferSelected), so
+-- those DO need to be defined here, otherwise `hi clear` leaves them empty and
+-- icon backgrounds render incorrectly.
+return {
+   ['BufferLineFill'] = { fg = c.fg, bg = c.alt_bg },
+   ['BufferLineBackground'] = { fg = c.fg, bg = c.tab },
+   ['BufferLineBufferVisible'] = { fg = c.fg, bg = c.tab },
+   ['BufferLineBufferSelected'] = { fg = c.fg, bg = c.selection_bg, italic = false },
+   ['BufferLineBuffer'] = { fg = c.fg, bg = c.tab },
+   ['BufferLineCloseButton'] = { fg = c.fg, bg = c.tab },
+   ['BufferLineCloseButtonSelected'] = { fg = c.fg, bg = c.selection_bg },
+   ['BufferLineCloseButtonVisible'] = { fg = c.fg, bg = c.tab },
+   ['BufferLineModified'] = { fg = c.fg, bg = c.tab },
+   ['BufferLineModifiedVisible'] = { fg = c.fg, bg = c.tab },
+   ['BufferLineModifiedSelected'] = { fg = c.fg, bg = c.selection_bg },
+   ['BufferLineDuplicateSelected'] = { fg = c.green, bg = c.selection_bg },
+   ['BufferLineDuplicateVisible'] = { fg = c.gray, bg = c.tab },
+   ['BufferLineDuplicate'] = { fg = c.green, bg = c.tab },
+   ['BufferLineSeparator'] = { fg = c.alt_bg, bg = c.alt_bg },
+   ['BufferLineSeparatorSelected'] = { fg = c.alt_bg, bg = c.alt_bg },
+   ['BufferLineSeparatorVisible'] = { fg = c.alt_bg, bg = c.tab },
+   ['BufferLineIndicatorSelected'] = { fg = c.fg, bg = c.selection_bg },
+   ['BufferLineIndicatorVisible'] = { fg = c.fg, bg = c.tab },
+}
